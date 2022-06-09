dad2d69d39c7065d18bcc72d128c8df522fe28d4
Naufal Algiffary <naufalgiffary@gmail.com>
2022-06-09 20:47:54 +0700

    [#3] Finish target data sub page

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,13 +2,15 @@ import React, { useState } from 'react';
 
 // components
 import Header from 'components/Header';
-import { Button, Grid, Tab, Tabs, Typography } from '@mui/material';
+import { Grid, Tab, Tabs, Typography } from '@mui/material';
 import FilterSection from './FilterSection';
 import InfoSection from './InfoSection';
 import ChartSection from './ChartSection';
 import InventorySection from './InventorySection';
-import { Icon } from '@iconify/react';
-import InfoIcon from '@iconify/icons-carbon/information-filled';
+
+import TargetDataTable from './TargetDataTable';
+import TargetDataInformation from './TargetDataInformation';
+import CustomPagination from 'components/Pagination';
 
 const menuList = [
   { value: 0, label: 'Produksi' },
@@ -77,6 +79,45 @@ const data = [
     pv: 4300
   }
 ];
+
+const sample = [
+  {
+    year: 2021,
+    detail: [
+      { month: 'Januari', target: '70.000' },
+      { month: 'Februari', target: '70.000' },
+      { month: 'Maret', target: '70.000' },
+      { month: 'April', target: '70.000' },
+      { month: 'Mei', target: '70.000' },
+      { month: 'Juni', target: '70.000' },
+      { month: 'Juli', target: '70.000' },
+      { month: 'Agustus', target: '70.000' },
+      { month: 'September', target: '70.000' },
+      { month: 'Oktober', target: '70.000' },
+      { month: 'November', target: '70.000' },
+      { month: 'Desember', target: '70.000' }
+    ]
+  },
+  {
+    year: 2020,
+    detail: [
+      { month: 'Januari', target: '70.000' },
+      { month: 'Februari', target: '70.000' },
+      { month: 'Maret', target: '70.000' },
+      { month: 'April', target: '70.000' },
+      { month: 'Mei', target: '70.000' },
+      { month: 'Juni', target: '70.000' },
+      { month: 'Juli', target: '70.000' },
+      { month: 'Agustus', target: '70.000' },
+      { month: 'September', target: '70.000' },
+      { month: 'Oktober', target: '70.000' },
+      { month: 'November', target: '70.000' },
+      { month: 'Desember', target: '70.000' }
+    ]
+  }
+];
+
+const targetTableHead = ['TAHUN', 'BULAN', 'TARGET', 'ACTION'];
 
 export default function Dashboard() {
   const [menuTab, setMenuTab] = useState(0);
@@ -169,40 +210,11 @@ export default function Dashboard() {
 
               <FilterSection subMenu={subMenu} handleChangeSubMenu={handleChangeSubMenu} />
 
-              <Grid
-                container
-                direction="row"
-                alignItems="center"
-                justifyContent="flex-start"
-                mt={3}
-              >
-                <Grid
-                  container
-                  direction="row"
-                  alignItems="center"
-                  justifyContent="flex-start"
-                  item
-                  width="80%"
-                  sx={{
-                    border: '1px solid #E0E0E0',
-                    borderRadius: '4px',
-                    padding: '16px 12px',
-                    background: '#F2F2F2'
-                  }}
-                >
-                  <Icon color="#3F48C0" height={18} icon={InfoIcon} />
-                  <Typography variant="body2" width="95%" ml={2}>
-                    Input data target untuk dapat membandingkan realisasi produksi tambang. Data
-                    target berdasarkan tahun dan bulan, Untuk menambahkan data target, silahkan klik
-                    tambah target
-                  </Typography>
-                </Grid>
-                <Grid width="20%" container alignItems="center" justifyContent="flex-end">
-                  <Button sx={{ width: '80%' }} variant="contained">
-                    Tambah Target
-                  </Button>
-                </Grid>
-              </Grid>
+              <TargetDataInformation />
+
+              <TargetDataTable sample={sample} targetTableHead={targetTableHead} />
+
+              <CustomPagination />
             </Grid>
           </>
         )}
