3e9eb5c77f62bbad4626b81cabd90a2a8d88bd18
primawk <pwkuntoro@gmail.com>
2022-06-28 23:04:32 +0700

    layout laporan lab

--- src/pages/LaporanLab/InputLaporanInternal.jsx
+++ src/pages/LaporanLab/InputLaporanInternal.jsx
@@ -33,103 +33,135 @@ const InputLaporanInternal = () => {
           borderRadius: '4px'
         }}
       >
-        <Grid item sx={{ backgroundColor: 'blue', height: '7%' }}>
+        <Grid item sx={{ height: '7%', borderBottom: 1, borderBottomColor: '#E0E0E0' }}>
           <h2 style={{ margin: '1rem 0.5rem 0.5rem 2rem' }}>Input Laporan Internal Lab</h2>
         </Grid>
-        <Grid item sx={{ backgroundColor: 'red', height: '25%' }}>
+        <Grid item sx={{ height: '25%', borderBottom: 1, borderBottomColor: '#E0E0E0' }}>
           <h4 style={{ margin: '1.5rem 0.5rem 0.5rem 2rem' }}>Informasi Sample</h4>
           <Grid container sx={{ display: 'flex', flexDirection: 'row' }}>
-            <Grid sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Grid
+              sx={{ display: 'flex', flexDirection: 'column', margin: '1.5rem 0.5rem 0.5rem 2rem' }}
+              xs={2}
+            >
               <Box>Tanggal</Box>
               <Box>Tanggal</Box>
             </Grid>
-            <Grid sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Grid
+              sx={{ display: 'flex', flexDirection: 'column', margin: '1.5rem 0.5rem 0.5rem 2rem' }}
+              xs={2}
+            >
               <Box>Bukit</Box>
               <Box>Pilih Bukit</Box>
             </Grid>
           </Grid>
           <Grid container sx={{ display: 'flex', flexDirection: 'row' }}>
-            <Grid sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Grid
+              sx={{ display: 'flex', flexDirection: 'column', margin: '1.5rem 0.5rem 0.5rem 2rem' }}
+              xs={2}
+            >
               <Box>Nama Perusahaan</Box>
               <Box>Nomor Kontak Pengaju Sample</Box>
             </Grid>
-            <Grid sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Grid
+              sx={{ display: 'flex', flexDirection: 'column', margin: '1.5rem 0.5rem 0.5rem 2rem' }}
+              xs={2}
+            >
               <Box>Nama Perusahaan</Box>
               <Box>Nomor Kontak Pengaju Sample</Box>
             </Grid>
           </Grid>
         </Grid>
-        <Grid item sx={{ backgroundColor: 'green' }}>
-          <h2>Hasil Analisa </h2>
+        <Grid item sx={{ height: '50%' }}>
+          <h2 style={{ margin: '1rem 0.5rem 0.5rem 2rem' }}>Hasil Analisa </h2>
           <Grid container sx={{ display: 'flex', flexDirection: 'row' }}>
-            <Grid sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Grid
+              sx={{ display: 'flex', flexDirection: 'column', margin: '1.5rem 0.5rem 0.5rem 2rem' }}
+            >
               <Box>Kode Sample</Box>
               <Box>Kode sample</Box>
             </Grid>
           </Grid>
-          <Grid
-            container
-            sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}
-          >
-            <Grid sx={{ display: 'flex', flexDirection: 'column' }}>
+          <Grid container sx={{ display: 'flex', flexDirection: 'row' }}>
+            <Grid
+              sx={{ display: 'flex', flexDirection: 'column', margin: '1.5rem 0.5rem 0.5rem 2rem' }}
+              xs={2}
+            >
               <Box>Kadar Ni</Box>
               <Box>Nilai Kadar</Box>
               <Box>Nilai Kadar</Box>
             </Grid>
-            <Grid sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Grid
+              sx={{ display: 'flex', flexDirection: 'column', margin: '1.5rem 0.5rem 0.5rem 2rem' }}
+              xs={2}
+            >
               <Box>Kadar Ni</Box>
               <Box>Nilai Kadar</Box>
               <Box>Nilai Kadar</Box>
             </Grid>
-            <Grid sx={{ display: 'flex', flexDirection: 'column' }}>
-              <Box>Kadar Ni</Box>
-              <Box>Nilai Kadar</Box>
-              <Box>Nilai Kadar</Box>
-            </Grid>
-          </Grid>
-          <Grid
-            container
-            sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}
-          >
-            <Grid sx={{ display: 'flex', flexDirection: 'column' }}>
-              <Box>Kadar Ni</Box>
-              <Box>Nilai Kadar</Box>
-              <Box>Nilai Kadar</Box>
-            </Grid>
-            <Grid sx={{ display: 'flex', flexDirection: 'column' }}>
-              <Box>Kadar Ni</Box>
-              <Box>Nilai Kadar</Box>
-              <Box>Nilai Kadar</Box>
-            </Grid>
-            <Grid sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Grid
+              sx={{ display: 'flex', flexDirection: 'column', margin: '1.5rem 0.5rem 0.5rem 2rem' }}
+              xs={2}
+            >
               <Box>Kadar Ni</Box>
               <Box>Nilai Kadar</Box>
               <Box>Nilai Kadar</Box>
             </Grid>
           </Grid>
-          <Grid
-            container
-            sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}
-          >
-            <Grid sx={{ display: 'flex', flexDirection: 'column' }}>
+          <Grid container sx={{ display: 'flex', flexDirection: 'row' }}>
+            <Grid
+              sx={{ display: 'flex', flexDirection: 'column', margin: '1.5rem 0.5rem 0.5rem 2rem' }}
+              xs={2}
+            >
               <Box>Kadar Ni</Box>
               <Box>Nilai Kadar</Box>
               <Box>Nilai Kadar</Box>
             </Grid>
-            <Grid sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Grid
+              sx={{ display: 'flex', flexDirection: 'column', margin: '1.5rem 0.5rem 0.5rem 2rem' }}
+              xs={2}
+            >
               <Box>Kadar Ni</Box>
               <Box>Nilai Kadar</Box>
               <Box>Nilai Kadar</Box>
             </Grid>
-            <Grid sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Grid
+              sx={{ display: 'flex', flexDirection: 'column', margin: '1.5rem 0.5rem 0.5rem 2rem' }}
+              xs={2}
+            >
+              <Box>Kadar Ni</Box>
+              <Box>Nilai Kadar</Box>
+              <Box>Nilai Kadar</Box>
+            </Grid>
+          </Grid>
+          <Grid container sx={{ display: 'flex', flexDirection: 'row' }}>
+            <Grid
+              sx={{ display: 'flex', flexDirection: 'column', margin: '1.5rem 0.5rem 0.5rem 2rem' }}
+              xs={2}
+            >
+              <Box>Kadar Ni</Box>
+              <Box>Nilai Kadar</Box>
+              <Box>Nilai Kadar</Box>
+            </Grid>
+            <Grid
+              sx={{ display: 'flex', flexDirection: 'column', margin: '1.5rem 0.5rem 0.5rem 2rem' }}
+              xs={2}
+            >
+              <Box>Kadar Ni</Box>
+              <Box>Nilai Kadar</Box>
+              <Box>Nilai Kadar</Box>
+            </Grid>
+            <Grid
+              sx={{ display: 'flex', flexDirection: 'column', margin: '1.5rem 0.5rem 0.5rem 2rem' }}
+              xs={2}
+            >
               <Box>Kadar Ni</Box>
               <Box>Nilai Kadar</Box>
               <Box>Nilai Kadar</Box>
             </Grid>
           </Grid>
         </Grid>
-        <Grid item sx={{ backgroundColor: 'yellow' }}>
-          <Grid container sx={{ justifyContent: 'center' }}>
+        <Grid item sx={{ borderTop: 1, borderTopColor: '#E0E0E0' }}>
+          <Grid container sx={{ marginTop: '1.5rem', justifyContent: 'center' }}>
             <Button variant="contained">Tambah Data</Button>
           </Grid>
         </Grid>
@@ -137,14 +169,26 @@ const InputLaporanInternal = () => {
       <div
         style={{
           position: 'fixed',
+          display: 'flex',
           bottom: '0px',
-          backgroundColor: 'green',
-          width: '100%'
+          backgroundColor: 'white',
+          width: '100%',
+          height: '4.875rem',
+          boxShadow: '4px -10px 24px rgba(0, 0, 0, 0.04)'
         }}
       >
-        <Grid container sx={{ justifyContent: 'flex-end' }}>
-        <Button>Back</Button>
-          <Button variant="contained">Submit Laporan</Button>
+        <Grid
+          container
+          sx={{ justifyContent: 'flex-end', alignItems: 'center', marginRight: '5rem' }}
+        >
+          <Grid item xs={0.6}>
+            <Button>Back</Button>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" sx={{ width: '130%' }}>
+              Submit Laporan
+            </Button>
+          </Grid>
         </Grid>
       </div>
     </div>
