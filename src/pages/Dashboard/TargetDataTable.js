dfee9f0480df65fbd850e8aeba3fea7ebc294017
primawk <pwkuntoro@gmail.com>
2022-07-04 20:13:04 +0700

    1 year 1 action

--- src/pages/Dashboard/TargetDataTable.js
+++ src/pages/Dashboard/TargetDataTable.js
@@ -50,33 +50,52 @@ const TargetDataTable = ({ sample, targetTableHead }) => {
                   <TableCell align="center" sx={{ border: '1px solid #F2F2F2', minWidth: '15vw' }}>
                     {detail.target}
                   </TableCell>
-                  <TableCell sx={{ border: '1px solid #F2F2F2' }}>
-                    <Grid container justifyContent="space-around" alignItems="center">
-                      <Grid item md={5} xs={12} padding="0.2em 0">
-                        <Button
-                          sx={{ background: '#E5E5FE', boxShadow: '0', color: '#3F48C0' }}
-                          fullWidth
-                          variant="contained"
-                        >
-                          <Icon style={{ fontSize: '17px', marginRight: '1rem' }} icon={EditIcon} />
-                          Edit Data
-                        </Button>
+                  {detail.month === 'Januari' ? (
+                    <TableCell
+                      sx={{
+                        border: '1px solid #F2F2F2',
+                        justifyContent: 'flex-start',
+                        alignItems: 'flex-start'
+                      }}
+                      rowSpan={item.detail.length}
+                    >
+                      <Grid
+                        container
+                        sx={{
+                          justifyContent: 'space-around',
+                          alignItems: 'flex-start',
+                          marginBottom: '40rem'
+                        }}
+                      >
+                        <Grid item md={5} xs={12} padding="0.2em 0">
+                          <Button
+                            sx={{ background: '#E5E5FE', boxShadow: '0', color: '#3F48C0' }}
+                            fullWidth
+                            variant="contained"
+                          >
+                            <Icon
+                              style={{ fontSize: '17px', marginRight: '1rem' }}
+                              icon={EditIcon}
+                            />
+                            Edit Data
+                          </Button>
+                        </Grid>
+                        <Grid item md={5} xs={12} padding="0.2em 0" sx={{ alignItems: 'center' }}>
+                          <Button
+                            sx={{ background: '#E5E5FE', boxShadow: '0', color: '#3F48C0' }}
+                            fullWidth
+                            variant="contained"
+                          >
+                            <Icon
+                              style={{ fontSize: '17px', marginRight: '1rem' }}
+                              icon={DeleteIcon}
+                            />
+                            Delete Data
+                          </Button>
+                        </Grid>
                       </Grid>
-                      <Grid item md={5} xs={12} padding="0.2em 0" sx={{ alignItems: 'center' }}>
-                        <Button
-                          sx={{ background: '#E5E5FE', boxShadow: '0', color: '#3F48C0' }}
-                          fullWidth
-                          variant="contained"
-                        >
-                          <Icon
-                            style={{ fontSize: '17px', marginRight: '1rem' }}
-                            icon={DeleteIcon}
-                          />
-                          Delete Data
-                        </Button>
-                      </Grid>
-                    </Grid>
-                  </TableCell>
+                    </TableCell>
+                  ) : null}
                 </TableRow>
               ))}
             </>
