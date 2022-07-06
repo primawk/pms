a44b11630fdd6305deaa567dae38a4ee6aefe511
primawk <pwkuntoro@gmail.com>
2022-07-06 23:01:53 +0700

    update the login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -19,68 +19,83 @@ const Login = () => {
   };
 
   return (
-    <Grid container component="main" sx={{ height: '100vh' }}>
-      <Grid container alignItems="center" justifyContent="center">
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            width: '60%',
-            height: '100%'
-          }}
+    <div style={{ overflow: 'hidden' }}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        {/* logo */}
+        <Grid
+          item
+          container
+          justifyContent="flex-end"
+          alignItems="center"
+          className="navbar-brand"
+          sx={{ display: 'inline-flex', height: '10%' }}
         >
-          <Typography variant="body2" alignSelf="flex-start" mb={1}>
-            Selamat Datang di
-          </Typography>
-          <Typography variant="h6" alignSelf="flex-start" mb={3}>
-            PMS Smart Dashboard,
-          </Typography>
-          <Typography variant="h6" alignSelf="flex-start" mb={3}>
-            Login
-          </Typography>
-          <Typography variant="body2" alignSelf="flex-start">
-            Silakan login untuk melanjutkan,
-          </Typography>
-          <Box component="form" noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              fullWidth
-              label="Username"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              fullWidth
-              name="password"
-              label="Password"
-              type={showPassword ? 'text' : 'password'}
-              autoComplete="current-password"
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton onClick={handleShowPassword} edge="end">
-                      {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
-                    </IconButton>
-                  </InputAdornment>
-                )
-              }}
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 8, mb: 2, height: '3em' }}
-            >
-              LOGIN
-            </Button>
+          <img src="/PMSLogo.png" alt="logo" style={{ marginLeft: '7rem', marginRight: '10px' }} />
+          <h4 style={{  }}>PMS Smart Dashboard</h4>
+        </Grid>
+        <Grid container alignItems="center" justifyContent="center">
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              width: '60%',
+              height: '80%',
+              marginBottom: '30rem'
+            }}
+          >
+            <Typography variant="body2" alignSelf="flex-start" mb={1} sx={{fontWeight: '700'}}>
+              Selamat Datang di
+            </Typography>
+            <Typography variant="h6" alignSelf="flex-start" mb={3}>
+              PMS Smart Dashboard,
+            </Typography>
+            <Typography variant="h6" alignSelf="flex-start" mb={3}>
+              Login
+            </Typography>
+            <Typography variant="body2" alignSelf="flex-start">
+              Silakan login untuk melanjutkan,
+            </Typography>
+            <Box component="form" noValidate sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                fullWidth
+                label="Username"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                fullWidth
+                name="password"
+                label="Password"
+                type={showPassword ? 'text' : 'password'}
+                autoComplete="current-password"
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton onClick={handleShowPassword} edge="end">
+                        {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                      </IconButton>
+                    </InputAdornment>
+                  )
+                }}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 8, mb: 2, height: '3em' }}
+              >
+                LOGIN
+              </Button>
+            </Box>
           </Box>
-        </Box>
+        </Grid>
       </Grid>
-    </Grid>
+    </div>
   );
 };
 
