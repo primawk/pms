735c046a1572e582eff97e65d3112b32555aded8
sirSyarif <arif09.sh@gmail.com>
2022-07-07 15:53:20 +0700

    add login service

--- src/services/UserManagementService.js
+++ src/services/UserManagementService.js
@@ -15,7 +15,6 @@ const getUser = ({ page, row = 10, role, search }) => {
   });
 };
 
-<<<<<<< HEAD
 const loginUser = ({ email, password }) => {
   return request(`${ACCOUNT_MODEL}/login`, {
     method: 'POST',
@@ -26,10 +25,6 @@ const loginUser = ({ email, password }) => {
 const UserManagementService = {
   getUser,
   loginUser
-=======
-const UserManagementService = {
-  getUser
->>>>>>> 62ae86f168d51c1f6aafcf52bfb6eb5ad6fff0f9
 };
 
 export default UserManagementService;
